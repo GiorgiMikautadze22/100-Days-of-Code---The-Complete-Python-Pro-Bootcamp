e7b42e0a2e49152018118941fe965f1b6339ef0c
Giorgi Mikautadze <giorgi.mikautadze2223@gmail.com>
2024-10-05 14:27:26 +0400

    day 14: higher or lower game

--- Day 14/Higher or Lower Project/main.py
+++ Day 14/Higher or Lower Project/main.py
@@ -5,3 +5,40 @@
 #TODO: Else stop the game and print the score.
 #TODO: Write a while loop that keeps asking higher or lower until game is over
 #TODO: After the game has ended ask the user if he wants to play again.
+
+import random
+import game_data
+
+def compare(celeb_1, celeb_2):
+    if celeb_1["follower_count"] > celeb_2["follower_count"]:
+        return 1
+    else:
+        return 2
+
+def higher_lower():
+    score = 0
+    game_over = False
+    celeb_1 = random.choice(game_data.data)
+    celeb_2 = random.choice(game_data.data)
+
+    while not game_over:
+        user_guess = int(input(f'Who has more followers {celeb_1["name"]} or {celeb_2["name"]}. Write "1" or "2" \n'))
+
+        if user_guess == compare(celeb_1, celeb_2):
+            score += 1
+            if user_guess == 1:
+                celeb_2 = random.choice(game_data.data)
+            else:
+                celeb_1 = random.choice(game_data.data)
+            print(f'You are correct. Your score {score}')
+        else:
+            print(f'You lost. Your score {score}')
+            game_over = True
+
+    play_again = input('Do you want to play again? y or n: ')
+    if play_again == 'y':
+        higher_lower()
+    else:
+        print('Good by')
+
+higher_lower()
