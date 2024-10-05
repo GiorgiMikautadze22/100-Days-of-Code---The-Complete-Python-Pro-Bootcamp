edc2156a3b7ffd19a2900def2216cb2fe6faafd3
Giorgi Mikautadze <giorgi.mikautadze2223@gmail.com>
2024-10-05 13:23:35 +0400

    feat: initialize

---
+++ Day 14/Higher or Lower Project/main.py
@@ -0,0 +1,7 @@
+#TODO: take 2 random celebrities from the data
+#TODO: Ask the user to guess which has more followers
+#TODO: Create function that compares and returns the which celebrity won
+#TODO: If the users guess equals to the output of the compare function then increase score count.
+#TODO: Else stop the game and print the score.
+#TODO: Write a while loop that keeps asking higher or lower until game is over
+#TODO: After the game has ended ask the user if he wants to play again.
